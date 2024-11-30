38a3213c7ee365b0d26dc0ca2527dc88d9195118
MoBeeRang <preepree@naver.com>
2024-11-30 11:29:05 +0900

    m

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,46 +7,47 @@ import CurrentWeather from '../components/CurrentWeather'
 import Closet from '../components/slider/Closet'
 import { Wrap, Main } from '../styles/StyledComponent'
 import Grid from '@mui/material/Grid2'
-import { fetchSearchWeather, ENDPOINTS } from '../features/weather/weatherSlice';
-import { fetchSearchImages } from '../features/closet/closetSlice';
-import useHeight from '../hooks/useHeight';
+import { fetchSearchWeather, ENDPOINTS } from '../features/weather/weatherSlice'
+import { fetchSearchImages } from '../features/closet/closetSlice'
+import useHeight from '../hooks/useHeight'
 
 function Home() {
    //params :: Menu의 Height, Footer의 Height
    const height = useHeight(57, 100)
 
-   const dispatch = useDispatch();
-   const { searchResults: weatherResult, loading: weatherLoading, error: weatherError, selectedCity, selectedPreset } = useSelector((state) => state.weather);
-   const { searchResults: closetResult, loading: closetLoading, error: closetError } = useSelector((state) => state.closet);
+   const dispatch = useDispatch()
+   const { searchResults: weatherResult, loading: weatherLoading, error: weatherError, selectedCity, selectedPreset } = useSelector((state) => state.weather)
+   const { searchResults: closetResult, loading: closetLoading, error: closetError } = useSelector((state) => state.closet)
 
-  useEffect(() => {
-    dispatch(fetchSearchWeather({ endpoint: ENDPOINTS.WEATHER, id: selectedCity }))
-  }, [dispatch, selectedCity])
-   
    useEffect(() => {
-    if (weatherResult && weatherResult.main) { // weatherResult와 main이 존재하는지 확인
-        console.log('weatherResult:', weatherResult);
-        console.log('Temperature:', weatherResult.main.temp);
+      dispatch(fetchSearchWeather({ endpoint: ENDPOINTS.WEATHER, id: selectedCity }))
+   }, [dispatch, selectedCity])
 
-        // weatherResult.main.temp를 이용하여 이미지 검색
-        dispatch(fetchSearchImages(weatherResult.main.temp));
-    }
-   }, [dispatch, weatherResult,selectedPreset])
+   useEffect(() => {
+      if (weatherResult && weatherResult.main) {
+         // weatherResult와 main이 존재하는지 확인
+         console.log('weatherResult:', weatherResult)
+         console.log('Temperature:', weatherResult.main.temp)
 
-  if ( weatherLoading || closetLoading ) return <p>loading</p>
-  if ( weatherError ) return <p>error:{ weatherError }</p>
-   if (closetError) return <p>error:{closetError}</p>
+         // weatherResult.main.temp를 이용하여 이미지 검색
+         dispatch(fetchSearchImages(weatherResult.main.temp))
+      }
+   }, [dispatch, weatherResult, selectedPreset])
+
+   if (weatherLoading || closetLoading) return <p>loading</p>
+   if (weatherError) return <p>error:{weatherError}</p>
+   if (closetError && closetError === 'Request failed with status code 403') return <p>error:{closetError}</p>
 
    return (
       <Wrap>
          <Menu />
          <Main $height={height}>
-            <Grid container spacing={1} style={{marginTop: '150px', alignItems: 'center'}}>
+            <Grid container spacing={1} style={{ marginTop: '150px', alignItems: 'center' }}>
                <Grid size={4}>
                   <CurrentWeather weatherResult={weatherResult} />
                </Grid>
                <Grid size={8}>
-                  <Closet closetResult={closetResult} ></Closet>
+                  <Closet closetResult={closetResult}></Closet>
                </Grid>
             </Grid>
          </Main>
